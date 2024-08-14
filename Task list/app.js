77e6967c6c49d4e2c25b39f9420d00fb0f8f3c78
Adela Ciucu <140815603+Adela-Ciucu@users.noreply.github.com>
2024-08-14 11:38:58 +0300

    Add files via upload
    Adding Delete and Filter task in JavaScript.

--- Task list/app.js
+++ Task list/app.js
@@ -9,6 +9,12 @@ loadEventListeners();
 function loadEventListeners() {
 
   form.addEventListener('submit', addTask);
+  
+  taskList.addEventListener('click', removeTask);
+  
+  clearBtn.addEventListener('click', clearTasks);
+  
+  filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e) {
@@ -36,3 +42,31 @@ function addTask(e) {
 
   e.preventDefault();
 }
+
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+function clearTasks() {
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+}
+
